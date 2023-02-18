4ac828d51d02860aa4ec4fbd1e941c0b6c6e2e78
Michael Kamara <mikekkamara@gmail.com>
2023-02-18 16:50:10 +0300

    clear local storage

--- resources/js/components/users/Search.vue
+++ resources/js/components/users/Search.vue
@@ -66,25 +66,7 @@ export default {
         ...mapGetters(["allShifts"])
     },
     mounted() {
-        if (!Object.keys(JSON.parse(localStorage.getItem('filters'))).includes('absent')) {
-            localStorage.removeItem('filters');
-        }
-        this.$store.subscribe((mutation, state) => {
-            if (mutation.type === 'setUsers') {
-                let storedFilters = localStorage.getItem('filters');
-                if (storedFilters) {
-
-                    // console.log(Object.keys(this.filters).includes('absent'))
-                    this.filters = JSON.parse(storedFilters);
-                }
-
-                let storedSearch = localStorage.getItem('search');
-
-                if (storedSearch) {
-                    this.search = storedSearch;
-                }
-            }
-        });
+        localStorage.clear();
 
     },
     methods: {
